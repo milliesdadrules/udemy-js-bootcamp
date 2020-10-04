e1603e3e00062db32f76c11ae2a4c7b56c6d1679
James McNeil <mcneil_james@hotmail.com>
2020-10-04 19:45:58 +0100

    module one challenge

---
+++ arrays/todo.js
@@ -0,0 +1,5 @@
+const todos = ["Tidy Room", "practice Piano","Hour of Code","Make Dinner","Clean Fish Tank"]
+
+console.log(`You have ${todos.length} todos still to be completed.`)
+console.log(`Todo: ${todos[0]}`)
+console.log(`Todo: ${todos[todos.length -2]}`)
