8c7f9abfa38d2cb2a797fadbe9ba860deba0d4b9
James McNeil <mcneil_james@hotmail.com>
2020-10-04 19:36:56 +0100

    module one

---
+++ arrays/notes.js
@@ -0,0 +1,7 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+
+const notes = ["Note 1","Note 2","Note 3"]
+
+console.log(notes.length)
+
+console.log(notes[0])
