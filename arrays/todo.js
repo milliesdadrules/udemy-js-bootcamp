f4646e670f903a7101b091f27e2481f63942652b
James McNeil <mcneil_james@hotmail.com>
2020-10-05 19:52:18 +0100

    tidy todos.js

--- arrays/todo.js
+++ arrays/todo.js
@@ -2,16 +2,11 @@ const todos = ["Tidy Room", "practice Piano","Hour of Code","Make Dinner","Clean
 
 // Delete 3rd item
 todos.splice(2,1)
-console.log(todos)
 
 // Add new item onto the end
 todos.push("Get fish food")
-console.log(todos)
 
 // remove first item from list
 todos.splice(0,1)
-console.log(todos)
-
 
 console.log(`You have ${todos.length} todos still to be completed.`)
-console.log(todos)
