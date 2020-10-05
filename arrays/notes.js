f8bc59fd0c3cb69445b3dfb47da2311d36a32f34
James McNeil <mcneil_james@hotmail.com>
2020-10-05 19:46:29 +0100

    arrays challenge

--- arrays/notes.js
+++ arrays/notes.js
@@ -1,7 +1,7 @@
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
 
 const notes = ["Note 1","Note 2","Note 3"]
-
+/*
 //  Removes the first element from an array and returns it
 console.log(notes.pop())
 
@@ -13,6 +13,13 @@ notes.unshift("My new first Note")
 
 // Appends new elements to an array, and returns the new length of the array.
 notes.push("Collect shopping")
+
+// Removes elements from an array and, if necessary, inserts new elements in their place, returning the deleted elements.
+// Array<string>.splice(start: number, deleteCount?: number): string[] (+1 overload)
+notes.splice(1,0,"Yes Son!!!")
+*/
+
+notes[0] = "This is now the new Note 1"
 console.log(notes.length)
 
 console.log(notes)
--- arrays/todo.js
+++ arrays/todo.js
@@ -1,5 +1,17 @@
 const todos = ["Tidy Room", "practice Piano","Hour of Code","Make Dinner","Clean Fish Tank"]
 
+// Delete 3rd item
+todos.splice(2,1)
+console.log(todos)
+
+// Add new item onto the end
+todos.push("Get fish food")
+console.log(todos)
+
+// remove first item from list
+todos.splice(0,1)
+console.log(todos)
+
+
 console.log(`You have ${todos.length} todos still to be completed.`)
-console.log(`Todo: ${todos[0]}`)
-console.log(`Todo: ${todos[todos.length -2]}`)
+console.log(todos)
