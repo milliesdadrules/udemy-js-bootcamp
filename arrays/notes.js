f81e73d0c49333a31cdfdce3166c49c17049acbf
James McNeil <mcneil_james@hotmail.com>
2020-10-05 19:49:53 +0100

    remove comment block notes.js

--- arrays/notes.js
+++ arrays/notes.js
@@ -1,7 +1,7 @@
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
 
 const notes = ["Note 1","Note 2","Note 3"]
-/*
+
 //  Removes the first element from an array and returns it
 console.log(notes.pop())
 
@@ -17,7 +17,7 @@ notes.push("Collect shopping")
 // Removes elements from an array and, if necessary, inserts new elements in their place, returning the deleted elements.
 // Array<string>.splice(start: number, deleteCount?: number): string[] (+1 overload)
 notes.splice(1,0,"Yes Son!!!")
-*/
+
 
 notes[0] = "This is now the new Note 1"
 console.log(notes.length)
