10b07e3b943c1bc4286657420c231eb89645a306
James McNeil <mcneil_james@hotmail.com>
2020-10-17 16:24:42 +0100

    sort challenge completed

--- arrays/todo.js
+++ arrays/todo.js
@@ -1,6 +1,6 @@
 const todos = [{
-    text:"Tidy Room",
-    completed: true
+    text: "Tidy Room",
+    completed: false
 }, {
     text: "practice Piano",
     completed: false
@@ -12,29 +12,40 @@ const todos = [{
     completed: true
 }, {
     text: "Clean Fish Tank",
-    completed: false
+    completed: true
 }]
 
-const deleteTodo = function(list, text){ // Create function that takes in arguements of array value (list) and query value (text)
-    const index = list.findIndex(function(list){ // create call back function to find the index where list contains search text
-        return list.text.toLowerCase() === text.toLowerCase()  // converts both contents and search to lowercase and returns index value
-    }) 
-    if (index > -1){ // if index value -1 then nothing
-        list.splice(index,1) // use splice to remove item from array. splice(starting point, number of items to remove)
-    }
+// const deleteTodo = function(list, text){ // Create function that takes in arguements of array value (list) and query value (text)
+//     const index = list.findIndex(function(list){ // create call back function to find the index where list contains search text
+//         return list.text.toLowerCase() === text.toLowerCase()  // converts both contents and search to lowercase and returns index value
+//     }) 
+//     if (index > -1){ // if index value -1 then nothing
+//         list.splice(index,1) // use splice to remove item from array. splice(starting point, number of items to remove)
+//     }
     
-}
-// console.log(todos)
-
-const getThingsTodo = function(todos){
-    return todos.filter(function(todoOutstanding,index){
-        return !todoOutstanding.completed
-    })
-}
-
-
-console.log(getThingsTodo(todos))
-
-
+// }
 // console.log(deleteTodo(todos, "make dinner"))
 // console.log(todos)
+
+// const getThingsTodo = function(todos){
+//     return todos.filter(function(todoOutstanding,index){
+//         return !todoOutstanding.completed
+//     })
+// }
+
+// console.log(getThingsTodo(todos))
+
+const sortTodos = function(todos){
+    todos.sort(function(a,b){
+        if(!a.completed && b.completed){
+            return -1
+        } else if (!b.completed && a.completed){
+            return 1
+        } else {
+            return 0
+        }
+    })
+}
+// console.log(todos)
+sortTodos(todos)
+console.log(todos)
