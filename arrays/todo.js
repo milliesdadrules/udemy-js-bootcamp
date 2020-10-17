07efd39c14c160ab0a71772b29e1e4423ce32f9a
James McNeil <mcneil_james@hotmail.com>
2020-10-17 12:40:14 +0100

    added comments to code

--- arrays/todo.js
+++ arrays/todo.js
@@ -15,15 +15,20 @@ const todos = [{
     completed: false
 }]
 
-const deleteTodo = function(list, text){
-    const index = list.findIndex(function(list){
-        return list.text.toLowerCase() === text.toLowerCase()
+const deleteTodo = function(list, text){ // Create function that takes in arguements of array value (list) and query value (text)
+    const index = list.findIndex(function(list){ // create call back function to find the index where list contains search text
+        return list.text.toLowerCase() === text.toLowerCase()  // converts both contents and search to lowercase and returns index value
     }) 
-    if (index > -1){
-        list.splice(index,1)
+    if (index > -1){ // if index value -1 then nothing
+        list.splice(index,1) // use splice to remove item from array. splice(starting point, number of items to remove)
     }
     
 }
+console.log(todos)
 
-console.log(deleteTodo(todos, "&&&make dinner"))
+
+// console.log(getThingsTodo(todos))
+
+
+console.log(deleteTodo(todos, "make dinner"))
 console.log(todos)
