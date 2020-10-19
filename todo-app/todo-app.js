296ed9e684c011ce49bbc75b7dacecc9d7fb7008
James McNeil <mcneil_james@hotmail.com>
2020-10-19 04:28:12 +0100

    todo challenge, add filter to todos

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -15,25 +15,51 @@ const todos = [{
     completed: true
 }]
 
+// 
+const todoFilter = {
+    todoSearch: ""
+}
+
+const renderTodos = function(todos,todoFilter){
+    const filteredTodos = todos.filter(function(todo){
+        return todo.text.toLowerCase().includes(todoFilter.todoSearch.toLowerCase())
+    })
+    console.log(filteredTodos)
+    document.querySelector("#list-todos").innerHTML = ""
+
+    filteredTodos.forEach(function(todo){
+        const todoResult = document.createElement("p")
+        todoResult.textContent = todo.text
+        document.querySelector("#list-todos").appendChild(todoResult)
+    })
+
+}
+renderTodos(todos,todoFilter)
+
+const searchTodos = document.querySelector("#search-todos").addEventListener("input", function(e){
+    todoFilter.todoSearch = e.target.value
+    // console.log(todoFilter.todoSearch)
+    renderTodos(todos,todoFilter)
+})
 // create new array filtered on completed value being false (length used to count outstanding tasks)
 const incompleteTodos = todos.filter(function(todo){
     return !todo.completed
 })
 
-// 
+// Display summary of outstanding tasks
 const summaryIncomplete = document.createElement('h2')
 summaryIncomplete.textContent = `You have ${incompleteTodos.length} todo's left`
 document.querySelector('#todo-summary').appendChild(summaryIncomplete)
 
 
 // do a forEach on array and conditionally on show incomplete tasks
-todos.forEach(function(task){
-    if(!task.completed){
-        const newTodo = document.createElement('p')
-        newTodo.textContent = task.text
-        document.querySelector('#list-todos').appendChild(newTodo)
-    }
-})
+// todos.forEach(function(task){
+//     if(!task.completed){
+//         const newTodo = document.createElement('p')
+//         newTodo.textContent = task.text
+//         document.querySelector('#list-todos').appendChild(newTodo)
+//     }
+// })
 
 // Listen for new todo creation
 document.querySelector("#add-todo").addEventListener("click", function(e){
