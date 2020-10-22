e977b5df0f01d048db83bc0ced9a3d2e48140c6d
James McNeil <mcneil_james@hotmail.com>
2020-10-22 21:15:20 +0100

    added uuid to todo

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -18,6 +18,7 @@ document.querySelector("#search-todos").addEventListener("input", function(e){
 document.querySelector("#todo-form").addEventListener("submit", function(e){
     e.preventDefault()
     todos.push({
+        id: uuidv4(),
         text: e.target.elements.newTodo.value,
         completed: false
     })
