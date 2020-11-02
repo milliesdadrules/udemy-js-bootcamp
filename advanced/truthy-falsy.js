03e5aa76953fef709a13aceae799252ce1240a51
James McNeil <mcneil_james@hotmail.com>
2020-11-02 17:27:12 +0000

    testing truthy falsy

--- advanced/truthy-falsy.js
+++ advanced/truthy-falsy.js
@@ -1,11 +1,15 @@
-const products = ['some item']
+const products = [{
+    name: "Computer Mouse"
+}]
 const product = products[0]
 
 // Truthy - Values that resolve to true in a boolean context
 // Falsy = Values that resolve to false in a boolean context
+// Falsy values - false, 0, empty string, null, undefined, NaN
 
-if(product !== undefined){
+if(product){
     console.log("product found!");
 } else {
     console.log("Product not found");
 }
+console.log(product);
