4dfc32c9688df77629b9bdc3ce2395b039c1f919
James McNeil <mcneil_james@hotmail.com>
2020-11-05 20:55:29 +0000

    updated person.js

--- oop/person.js
+++ oop/person.js
@@ -1,13 +1,25 @@
-const Person = function(firstName,lastName, age){
+const Person = function(firstName,lastName, age, likes=[]){
     this.firstName = firstName,
     this.lastName = lastName,
     this.age = age
+    this.likes = likes
 }
 
-const me = new Person("James","McNeil",47)
 
+Person.prototype.getBio = function(){
+    return `${this.firstName} is ${this.age}`
+}
 
-console.log(me);
+Person.prototype.setName = function(fullName){
+    const names = fullName.split(" ")
+    this.firstName = names[0]
+    this.lastName = names[1]
+}
+
+const me = new Person("James","McNeil",47, ["Coding","Piano"])
+me.setName("Jess McNeil")
+console.log(me.getBio());
+
 
 const meToo = new Person("Millie","McNeil",11)
-console.log(meToo);
+console.log(meToo.getBio());
