7f0ae76ff11a1c3c72839f76cddbf795c9e22e2d
James McNeil <mcneil_james@hotmail.com>
2020-11-06 18:42:50 +0000

    hangman challenge 1

--- oop/hangman.js
+++ oop/hangman.js
@@ -1,10 +1,24 @@
 const Hangman = function (word,remainingGuesses) {
-    this.word = word,
+    this.word = word.toLowerCase().split("")
+    this.guesses = ["s","l","r","t","o"]
     this.remainingGuesses = remainingGuesses
 }
 
-const game = new Hangman("Cat",5)
-console.log(game);
+Hangman.prototype.getPuzzle = function(){
+    let puzzle = ""
+    this.word.forEach((letter) => {
 
-const game2 = new Hangman("James Bond",9)
-console.log(game2);
+        if(this.guesses.includes(letter) || letter === " "){
+            puzzle += letter
+        } else {
+            puzzle += "*"
+        }
+
+    })
+    return puzzle
+}
+const game = new Hangman("Trolls 2",5)
+console.log(game.getPuzzle());
+
+// const game2 = new Hangman("James Bond",9)
+// console.log(game2);
