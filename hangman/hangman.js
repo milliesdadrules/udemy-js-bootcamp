6a754758f6758cc8758188e9563b5a221e1dbaf5
James McNeil <mcneil_james@hotmail.com>
2020-11-06 20:41:38 +0000

    hangman challenge 2

--- hangman/hangman.js
+++ hangman/hangman.js
@@ -1,14 +1,26 @@
 const Hangman = function (word,remainingGuesses) {
     this.word = word.toLowerCase().split("")
-    this.guesses = ["s","l","r","t","o"]
+    this.lettersGuessed = []
     this.remainingGuesses = remainingGuesses
+}
+
+Hangman.prototype.makeGuess = function(guess){
+    guess = guess.toLowerCase()
+    const isUnique = !this.lettersGuessed.includes(guess)
+    const isBadGuess =  !this.word.includes(guess)
+    if(isUnique){
+        this.lettersGuessed.push(guess)
+    }
+    if(isUnique && isBadGuess){
+        this.remainingGuesses--
+    }
 }
 
 Hangman.prototype.getPuzzle = function(){
     let puzzle = ""
     this.word.forEach((letter) => {
 
-        if(this.guesses.includes(letter) || letter === " "){
+        if(this.lettersGuessed.includes(letter) || letter === " "){
             puzzle += letter
         } else {
             puzzle += "*"
@@ -17,8 +29,14 @@ Hangman.prototype.getPuzzle = function(){
     })
     return puzzle
 }
-const game = new Hangman("Trolls 2",5)
+const game = new Hangman("Cat",2)
+game.makeGuess("c")
+game.makeGuess("t")
+game.makeGuess("y")
 console.log(game.getPuzzle());
+console.log(`You have ${game.remainingGuesses} remaining.`);
 
-const game2 = new Hangman("James Bond",9)
+const game2 = new Hangman("New Jersey",4)
+game2.makeGuess("w")
 console.log(game2.getPuzzle());
+console.log(`You have ${game2.remainingGuesses} remaining.`);
