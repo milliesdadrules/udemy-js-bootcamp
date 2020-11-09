a61c86bcb4a2fd021d7976e9eae928334b833784
James McNeil <mcneil_james@hotmail.com>
2020-11-09 19:02:28 +0000

    XMLHttpRequest test

--- hangman/app.js
+++ hangman/app.js
@@ -11,3 +11,17 @@ window.addEventListener("keypress", (e) => {
     wordEl.textContent = game.puzzle
     statusEl.textContent = game.statusMessage
 })
+
+// Making an an HTTP request
+
+const request = new XMLHttpRequest()
+
+request.addEventListener("readystatechange",(e) =>{
+    if(e.target.readyState){
+        const data = JSON.parse(e.target.responseText)
+        console.log(data);
+    }
+})
+request.open("GET","http://puzzle.mead.io/puzzle")
+request.send()
+//console.log();
