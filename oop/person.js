d444c0af2d02401906179fe982916b6d82303135
James McNeil <jmcneil@north-ayrshire.gov.uk>
2020-11-09 16:57:33 +0000

    used person.js to test getters and setters

--- oop/person.js
+++ oop/person.js
@@ -12,10 +12,25 @@ class Person {
         })
         return bio
     }
-    setName(fullName){
+    set fullName(fullName){
         const names = fullName.split(" ")
         this.firstName = names[0]
         this.lastName = names[1]
+    }
+    get fullName() {
+        return `${this.firstName} ${this.lastName}`
+    }
+}
+class Employee extends Person {
+    constructor(firstName,lastName,age,position, likes){
+        super(firstName,lastName,age,likes)
+        this.position = position
+    }
+    getBio(){
+        return `${this.fullName} is a ${this.position}.`
+    }
+    getYearsLeft(){
+        return 65 - this.age
     }
 }
 
@@ -33,56 +48,6 @@ class Student extends Person {
     }
 }
 
-const james = new Student("James","McNeil",47,70,["Coding","Running"])
-console.log(james.getBio())
-console.log(james.updateGrade(-25))
-console.log(james.getBio())
-
-
-
-class Employee extends Person {
-    constructor(firstName,lastName,age,position, likes){
-        super(firstName,lastName,age,likes)
-        this.position = position
-    }
-    getBio(){
-        return `${this.firstName} ${this.lastName} is a ${this.position}.`
-    }
-    getYearsLeft(){
-        return 65 - this.age
-    }
-}
-
-//#region 
-/*
-const Person = function(firstName,lastName, age, likes=[]){
-    this.firstName = firstName,
-    this.lastName = lastName,
-    this.age = age
-    this.likes = likes
-}
-
-
-Person.prototype.getBio = function(){
-    let bio = `${this.firstName} is ${this.age}.`
-    this.likes.forEach((like) => {
-        bio += `${this.firstName} likes ${like}.`
-    })
-    return bio
-}
-
-Person.prototype.setName = function(fullName){
-    const names = fullName.split(" ")
-    this.firstName = names[0]
-    this.lastName = names[1]
-}
-*/
-//#endregion
-
-// const me = new Employee("James","McNeil",47,"Coder" ,["Coding","Piano"])
-// me.setName("Jess McNeil")
-// console.log(me.getBio());
-// console.log(me.getYearsLeft());
-
-// const meToo = new Person("Millie","McNeil",11,["Make Up","Gaming"])
-// console.log(meToo.getBio());
+const me = new Employee("James","McNeil",47,"Coder",["Coding","Running"])
+me.fullName = "Mike Smith"
+console.log(me.getBio())
