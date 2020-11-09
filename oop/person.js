8cfa23007187219784baae5eb267017e7e1dd959
James McNeil <jmcneil@north-ayrshire.gov.uk>
2020-11-09 15:12:24 +0000

    changed person.js to update to new class syntax

--- oop/person.js
+++ oop/person.js
@@ -1,3 +1,26 @@
+class Person {
+    constructor(firstName,lastName, age, likes=[]){
+        this.firstName = firstName,
+        this.lastName = lastName,
+        this.age = age
+        this.likes = likes
+    }
+    getBio(){
+        let bio = `${this.firstName} is ${this.age}.`
+        this.likes.forEach((like) => {
+            bio += ` ${this.firstName} likes ${like}.`
+        })
+        return bio
+    }
+    setName(fullName){
+        const names = fullName.split(" ")
+        this.firstName = names[0]
+        this.lastName = names[1]
+    }
+}
+
+//#region 
+/*
 const Person = function(firstName,lastName, age, likes=[]){
     this.firstName = firstName,
     this.lastName = lastName,
@@ -19,6 +42,8 @@ Person.prototype.setName = function(fullName){
     this.firstName = names[0]
     this.lastName = names[1]
 }
+*/
+//#endregion
 
 const me = new Person("James","McNeil",47, ["Coding","Piano"])
 me.setName("Jess McNeil")
