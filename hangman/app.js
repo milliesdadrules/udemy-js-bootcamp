74fbc5bb42ac64da339cf1a2d3dd0500314a4c6c
James McNeil <mcneil_james@hotmail.com>
2020-11-10 20:05:09 +0000

    XMLHttpRequest app.jschallenge

--- hangman/app.js
+++ hangman/app.js
@@ -14,14 +14,30 @@ window.addEventListener("keypress", (e) => {
 
 // Making an an HTTP request
 
-const request = new XMLHttpRequest()
+// const request = new XMLHttpRequest()
 
-request.addEventListener("readystatechange",(e) =>{
-    if(e.target.readyState){
+// request.addEventListener("readystatechange",(e) =>{
+//     if(e.target.readyState === 4 && e.target.status === 200){
+//         const data = JSON.parse(e.target.responseText)
+//         console.log(data)
+//     } else if (e.target.readyState === 4) {
+//         console.log("An error has taken place")
+//     }
+// })
+
+// request.open("GET","http://puzzle.mead.io/puzzle?wordCount=3")
+// request.send()
+
+const countryCode = "GB"
+const request = new XMLHttpRequest()
+request.addEventListener("readystatechange", (e) => {
+    if(e.target.readyState === 4 && e.target.status === 200){
         const data = JSON.parse(e.target.responseText)
-        console.log(data);
+        const country = data.find((country) => country.alpha2Code === countryCode)
+        console.log(country.name)
+    } else if (e.target.readyState === 4){
+        console.log("Unable to fetch data");
     }
 })
-request.open("GET","http://puzzle.mead.io/puzzle")
+request.open("GET","http://restcountries.eu/rest/v2/all")
 request.send()
-//console.log();
