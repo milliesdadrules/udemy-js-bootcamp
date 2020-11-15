9f2df8218aededc72102eeab6c6a9ea3354b1a0c
James McNeil <mcneil_james@hotmail.com>
2020-11-15 15:13:33 +0000

    added validation to todo-app.js

--- todo-app/scripts/todo-app.js
+++ todo-app/scripts/todo-app.js
@@ -17,15 +17,18 @@ document.querySelector("#search-todos").addEventListener("input", (e) => {
 })
 
 document.querySelector("#todo-form").addEventListener("submit", (e) => {
+    const text = e.target.elements.newTodo.value.trim()
     e.preventDefault()
-    todos.push({
-        id: uuidv4(),
-        text: e.target.elements.newTodo.value,
-        completed: false
-    })
-    saveTodos(todos)
-    renderTodos(todos, todoFilter)
-    e.target.elements.newTodo.value= ""
+    if(text.trim().length > 0){
+        todos.push({
+            id: uuidv4(),
+            text: text,
+            completed: false
+        })
+        saveTodos(todos)
+        renderTodos(todos, todoFilter)
+        e.target.elements.newTodo.value= ""
+    }
 })
 
 document.querySelector("#hide-complete").addEventListener("change", (e) => {
