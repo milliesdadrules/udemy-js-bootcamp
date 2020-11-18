1998f430a8a27b236914a06a1ad64e10d8f4240d
James McNeil <mcneil_james@hotmail.com>
2020-11-18 18:27:09 +0000

    add uuid & moment to packages.json add filters.js

--- notes-app/src/index.js
+++ notes-app/src/index.js
@@ -1,1 +1,17 @@
-console.log("index.js");
+import { getNotes, createNote, removeNote, sortNotes, updateNote } from './notes.js'
+import {getFilters, setFilters} from './filters'
+// console.log(getNotes())
+// createNote()
+//removeNote("3c02a717-02fb-4c85-a32f-63832a6040d0")
+// updateNote("5d8c3bf2-c049-40eb-85e9-13784256a0ce",{
+//     title: "This is my note title",
+//     body: "This is the body of the note"
+// })
+
+
+console.log(getFilters())
+setFilters({
+    searchText: "Office",
+    sortBy: "byCreated"
+})
+console.log(getFilters())
---
+++ notes-app/src/notes.js
@@ -0,0 +1,104 @@
+import { v4 as uuidv4 } from 'uuid'
+import moment from 'moment'
+let notes = []
+const timestamp = moment().valueOf()
+// Read existing notes from localStorage
+const loadNotes = () => {
+    const notesJSON = localStorage.getItem("notes")
+    try {
+        return notesJSON ? JSON.parse(notesJSON) : []
+    } catch (error) {
+        return []
+    }
+    
+}
+
+// Save the notes to local storage
+const saveNotes = () => {
+    localStorage.setItem("notes", JSON.stringify(notes))
+}
+
+// expose notes from module
+const getNotes = () => notes
+
+const createNote = () => {
+    const noteID = uuidv4()
+    notes.push({
+        id: noteID,
+        title: "",
+        body: "",
+        createdAt: timestamp,
+        updatedAt: timestamp
+    })
+    saveNotes()
+}
+
+// Remove a note from the list
+const removeNote = (noteID) => {
+    const noteIndex = notes.findIndex((note) => note.id == noteID)
+
+    if(noteIndex > -1){
+        notes.splice(noteIndex, 1)
+        saveNotes()
+    }
+}
+
+// sort you notes by one of three ways
+const sortNotes = (sortBy) => {
+    if(sortBy === "byEdited"){
+        return notes.sort((a,b) => {
+            if(a.updatedAt > b.updatedAt){
+                return -1
+            } else if (a.updatedAt < b.updatedAt) {
+                return 1    
+            } else {
+                return 0
+            }
+        })
+    } else if( sortBy === "byCreated"){
+        return notes.sort((a,b) => {
+            if(a.createdAt > b.createdAt){
+                return -1
+            } else if (a.createdAt < b.createdAt) {
+                return 1    
+            } else {
+                return 0
+            }
+        })
+    } else if (sortBy === "alphabetical"){
+        return notes.sort((a,b) => {
+            if(a.title.toLowerCase() < b.title.toLowerCase()){
+                return -1
+            } else if (b.title.toLowerCase() < a.title.toLowerCase()) {
+                return 1    
+            } else {
+                return 0
+            }
+        })
+    } else {
+        return notes
+    }
+}
+
+const updateNote = (id, updates) =>{
+    const note = notes.find((note) => note.id === id)
+
+    if(!note){
+        return
+    }
+
+    if(typeof updates.title === 'string'){
+        note.title = updates.title
+        note.updatedAt = moment().valueOf()
+    }
+
+    if(typeof updates.body === 'string'){
+        note.body = updates.body
+        note.updatedAt = moment().valueOf()
+    }
+    saveNotes()
+}
+
+notes = loadNotes()
+
+export { getNotes, createNote, removeNote, sortNotes, updateNote }
